ff525ad5d0548315ae8940703089addf36858e93
Sajedul-Islam <sajedulislam.cse@gmail.com>
2022-04-04 20:02:31 +0600

    product review added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import "./Home.css";
+import UseProduct from './../Hooks/UseProduct';
+import ProductReview from '../ProductReview/ProductReview';
 
 const Home = () => {
+    //------------Has imported from custom hooks------------
+    const [products, setProducts] = UseProduct();
     return (
         <div>
             <div className='home-page-style'>
@@ -11,13 +15,24 @@ const Home = () => {
                         of this shoes.high quality fiber fabric bonding latest designing also more and more
                         parts are there.we provide 100% users security that you
                         extra power for showing you.we think it's your dreams shoes 'catch it'.</h5>
+                    <div className='btn-div'>
+                        <button className='btn-style'><a href="https://media.wired.com/photos/59327c679be5e55af6c25666/3:2/w_1280%2Cc_limit/vivo.jpeg" target="_blank">Live demo</a></button>
+                    </div>
                 </div>
                 <div className='image'>
                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrQYP14pPbiprZl22jNQWMgRWMnHbLSS7eXA&usqp=CAU" alt="images" />
                 </div>
             </div>
-            <div className='btn-div'>
-                <button className='btn-style'><a href="https://media.wired.com/photos/59327c679be5e55af6c25666/3:2/w_1280%2Cc_limit/vivo.jpeg" target="_blank">Live demo</a></button>
+            <div className='product-parent-div'>
+                <h3>Customer reviews</h3>
+                <div className='product'>
+                    {
+                        products.map(product => <ProductReview key={product.id} product={product}></ProductReview>)
+                    }
+                </div>
+            </div>
+            <div className='review-btn'>
+                <button>See all reviews</button>
             </div>
         </div>
     );
---
+++ src/components/ProductReview/ProductReview.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./ProductReview.css";
+
+const ProductReview = ({ product }) => {
+    const { name, img, review, ratings } = product;
+    return (
+        <div className='product-review'>
+            <img src={img} alt="images" />
+            <div className='product-description'>
+                <h6>{name}</h6>
+                <p className='ratings'>Review: <small>{review}</small></p>
+                <p className='ratings'>Ratings: <small>{ratings}</small></p>
+            </div>
+        </div>
+    );
+};
+
+export default ProductReview;
